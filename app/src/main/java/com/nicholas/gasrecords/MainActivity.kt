cf6fefdf02efc646751fe40b4f0aeb8002e93896
Nicholas <nickwtayl@gmail.com>
2019-10-06 18:30:55 -0400

    ok all this seems a bit closer

--- app/src/main/java/com/nicholas/gasrecords/MainActivity.kt
+++ app/src/main/java/com/nicholas/gasrecords/MainActivity.kt
@@ -10,8 +10,19 @@ import android.view.View
 import android.widget.EditText
 
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.common.api.ApiException
+import android.util.Log
+import kotlinx.android.synthetic.main.content_main.*
+
 
 const val EXTRA_MESSAGE = "com.nicholas.gasrecords.MESSAGE"
+const val RC_SIGN_IN = 9001
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +34,67 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        sign_in_button.visibility = View.VISIBLE
+        signed_in_textview.visibility = View.GONE
+
+        sign_in_button.setSize(SignInButton.SIZE_STANDARD)
+
+        sign_in_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null) {
+            val personName          = account.displayName
+            val personGivenName     = account.givenName
+            val personFamilyName    = account.familyName
+            val personEmail         = account.email
+            val personId            = account.id
+            val personPhoto         = account.photoUrl
+            updateUI(account)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount?) {
+        if (account != null) {
+            sign_in_button.visibility = View.GONE
+            signed_in_textview.visibility = View.VISIBLE
+            signed_in_textview.text = resources.getString(R.string.textview_signed_in, account.displayName)
+        } else {
+            sign_in_button.visibility = View.VISIBLE
+            signed_in_textview.visibility = View.GONE
         }
     }
 
@@ -46,15 +118,10 @@ class MainActivity : AppCompatActivity() {
      * send gas refill/mileage data to Google Sheets
      */
     fun sendData(view: View) {
-        val editMileageText     = findViewById<EditText>(R.id.editMileage)
-        val editGallonsText     = findViewById<EditText>(R.id.editGallons)
-        val editTotalText       = findViewById<EditText>(R.id.editTotal)
-        val editPricePerText    = findViewById<EditText>(R.id.editPricePer)
-
-        val mileage     = editMileageText.text.toString()
-        val gallons     = editGallonsText.text.toString()
-        val total       = editTotalText.text.toString()
-        val pricePer    = editPricePerText.text.toString()
+        val mileage     = editMileage.text.toString()
+        val gallons     = editGallons.text.toString()
+        val total       = editTotal.text.toString()
+        val pricePer    = editPricePer.text.toString()
 
         val message = "Data: $mileage $gallons $total $pricePer"
 
